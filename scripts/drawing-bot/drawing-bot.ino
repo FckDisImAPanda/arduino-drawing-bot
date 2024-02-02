46ec5e17292d89ed41e0e237864c544b1ad95d07
FckDisImAPanda <42lukasbetz42@gmail.com>
2024-02-03 00:04:13 +0100

    update code comments

--- scripts/drawing-bot/drawing-bot.ino
+++ scripts/drawing-bot/drawing-bot.ino
@@ -1,11 +1,11 @@
-// Include Servo.h libary
+// Include servo libary
 # include "Servo.h"
 
-// Motor definition
+// Definition of both motors
 Servo Motor_1;
 Servo Motor_2;
 
-// Save potentiometer values
+// Potentiometer values
 int InputValue_1;
 int InputValue_2;
 
@@ -15,18 +15,18 @@ int ServoPosition_2;
 
 void setup()  
 {
-  // Attacg motors to pins 9 and 10
+  // Attach motors to pins 9 and 10
   Motor_1.attach(9); 
   Motor_2.attach(10);
 }
 
 void loop()  
 {
-  // Get potentiometer value
+  // Get potentiometer value from pins A1 and A2
   int InputValue_1 = analogRead(A1);
   int InputValue_2 = analogRead(A2);
 
-  // Convert potentiometer values (0-1023) to the servo motor position (0-180)   
+  // Convert potentiometer values (0-1023) to the servo motor position (0°-180°)   
   ServoPosition_1 = map(InputValue_1, 0, 1023, 0, 180);
   ServoPosition_2 = map(InputValue_2, 0, 1023, 0, 180);
 
